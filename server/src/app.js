042f974a81671149107f478905fca439b2b97bd5
deepthi308 <deepthih6@gmail.com>
2024-10-30 03:06:22 +0530

    adding middlewares to the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,56 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+require("dotenv").config();
 
+// Create express server
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the server");
+// Morgan
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Helmet
+app.use(helmet());
+
+// Parse JSON data from request body
+app.use(express.json());
+
+// Parse JSON data from request URL
+app.use(express.urlencoded({ extended: true }));
+
+// Sanitize request data
+app.use(mongoSanitize());
+
+// Enable cookie parser
+app.use(cookieParser());
+
+// Gzip compression
+app.use(compression());
+
+// File upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Cors
+app.use(
+  // cors({
+  //   origin: "http://localhost:3000",
+  // })
+  cors()
+);
+
+app.post("/", (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
-require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
   console.log(`Server has started listening at http://localhost:${PORT}...`);
 });

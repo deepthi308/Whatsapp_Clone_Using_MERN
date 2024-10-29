8a3597d61ce251865ba33eeac0e2b249074680d5
deepthi308 <deepthih6@gmail.com>
2024-10-30 03:59:28 +0530

    custom logger

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
+const logger = require("./configs/logger.config");
 
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
-  console.log(`Server has started listening at http://localhost:${PORT}...`);
+  logger.info(`Server has started listening at http://localhost:${PORT}...`);
 });
